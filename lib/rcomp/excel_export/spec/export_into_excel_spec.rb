aab4e32e87810580d2fccff55ee56eada6785b46
Sven Suska <sven838git@suska.org>
2009-08-25 15:28:51 +0200

    simple tests mit typumwandlung

---
+++ lib/rcomp/excel_export/spec/export_into_excel_spec.rb
@@ -0,0 +1,57 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+require 'spreadsheet'
+require 'export_into_excel'
+require File.dirname(__FILE__) + '/fixtures/daten_modell_1'
+
+
+describe ExportIntoExcel do
+  filepath = File.dirname(File.dirname(__FILE__)) + "/temp/test_temp/test_data.xls" #.gsub('\\','/')
+  daten_modell_dummy = DatenModellDummy.new
+  File.delete(filepath) rescue nil
+  @export_into_excel = ExportIntoExcel.new(filepath)
+  @export_into_excel.exportieren(daten_modell_dummy)
+  before(:each) do
+    
+  end
+
+  it "xls datei angelegt?" do
+    File.exist?(filepath).should == true
+  end
+
+  it "sollte integer auslesen" do
+    z = 1
+    s = 1
+    book = Spreadsheet.open filepath
+    book.worksheet(0).row(z+1)[s].should == daten_modell_dummy.getValueAt(z,s).to_i
+  end
+
+  it "sollte float auslesen" do
+    z = 2
+    s = 1
+    book = Spreadsheet.open filepath
+    book.worksheet(0).row(z+1)[s].should == daten_modell_dummy.getValueAt(z,s).to_f
+  end
+
+  it "sollte strings auslesen" do
+    z, s = 2, 3 #TODO: beim refactoring drauf achten
+    book = Spreadsheet.open filepath
+    book.worksheet(0).row(z+1)[s].should == daten_modell_dummy.getValueAt(z,s).to_s
+  end
+
+  it "sollte nil auslesen" do
+    z = 4
+    s = 4
+    book = Spreadsheet.open filepath
+    book.worksheet(0).row(z+1)[s].should == daten_modell_dummy.getValueAt(z,s)
+  end
+
+  it "sollte spaltennamen auslesen" do
+    book = Spreadsheet.open filepath
+    book.worksheet(0).row(0)[2].should == daten_modell_dummy.getColumnName(2)
+  end
+
+
+end
+
+
